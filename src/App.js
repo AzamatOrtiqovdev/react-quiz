e50f1e8719ab636619b24ce6a743ec510dade762
AzamatOrtiqovdev <ortiqovazamat890@gmail.com>
2024-09-03 12:03:46 +0500

    getting data from json server

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { useEffect } from "react";
 import Header from "./Header";
-import Main from "./Main"
+import Main from "./Main";
 
 export default function App() {
-  return <div className="app">
-    <Header />
-    <Main>
-      <p>1/15</p>
-      <p>Question ?</p>
-    </Main>
-  </div>;
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question ?</p>
+      </Main>
+    </div>
+  );
 }
